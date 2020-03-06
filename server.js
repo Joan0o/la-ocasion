2b091c0854d6ce0c5c9728e506aa3d029feb932c
tatis <shuan779@gmail.com>
2020-03-06 16:18:20 -0500

    asd

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+app.use(express.json())
+
+const itemsRouter = require('./routes/items.js')
+app.use('/items', itemsRouter)
+
+app.listen(3000, () => console.log('server running'))
