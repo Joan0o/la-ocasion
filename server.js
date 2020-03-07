0a754acc7494ed5017b303ebaeda8a45e9afe506
joan <shuan779@gmail.com>
2020-03-07 02:24:15 -0500

    react

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 
+const path = require('path')
+
 const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose')
+
+const DIST_DIR = path.join(__dirname, './dist'); // NEW
+const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
+
+app.use(express.static(DIST_DIR)); // NEW
+
+app.get('/', (req, res) => res.sendFile(HTML_FILE))
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 
